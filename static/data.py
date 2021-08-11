b31cd291eae97b761343087f588d6b4b8ba11fc2
Zach Anderson <65784457+zmoney20@users.noreply.github.com>
2021-08-11 15:17:46 -0600

    added additional data

--- static/data.py
+++ static/data.py
@@ -1,8 +1,32 @@
 project_data = [
     {
-        'title': 'poop',
+    "title": "React Portfolio",
+    "image": "react-portfolio.png",
+    "description": "Web Portfolio made with React",
+    "url": "zach-anderson-react.netlify.app"
     },
     {
-        'title': 'poop2'
+    "title": "React PhotoWall Web App",
+    "image": "photowall.png",
+    "description": "Photowall Web app using React and Redux",
+    "url": "https://github.com/zmoney20/photowall"
+    },
+    {
+    "title": "Node.js Sample Profile Wesbite",
+    "image": "nodejsproject.png",
+    "description": "Sample profile website made with Node.js",
+    "url": "https://github.com/zmoney20/sample-project"
+    },
+    {
+    "title": "Space Invaders",
+    "image": "space-invaders.png",
+    "description": "Pew pew the alien invaders!",
+    "url": "https://github.com/zmoney20/space-invaders"
+    },
+    {
+    "title": "Fall Game",
+    "image": "fall-game.png",
+    "description": "Play as an 8-bit character sliding and falling into the abyss.",
+    "url": "https://github.com/zmoney20/Fall_game"
     }
 ]

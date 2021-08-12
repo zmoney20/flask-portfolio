1c1d712aa8b9ca65ddc0852cba49afc7a352f6ee
Zach Anderson <65784457+zmoney20@users.noreply.github.com>
2021-08-12 11:23:33 -0600

    fixed up data just a lil bit

--- static/data.py
+++ static/data.py
@@ -1,33 +1,33 @@
 project_data = [
     {
-    "title": "React Portfolio",
-    "image": "react-portfolio.png",
-    "description": "Web Portfolio made with React",
-    "url": "zach-anderson-react.netlify.app"
+        "title": "React Portfolio",
+        "image": "react-portfolio.png",
+        "description": "Web Portfolio made with React",
+        "url": "https://zach-anderson-react.netlify.app"
     },
     {
-    "title": "React PhotoWall Web App",
-    "image": "photowall.png",
-    "description": "Photowall Web app using React and Redux",
-    "url": "https://github.com/zmoney20/photowall"
+        "title": "React PhotoWall Web App",
+        "image": "photowall.png",
+        "description": "Photowall Web app using React and Redux",
+        "url": "https://github.com/zmoney20/photowall"
     },
     {
-    "title": "Node.js Sample Profile Wesbite",
-    "image": "nodejsproject.png",
-    "description": "Sample profile website made with Node.js",
-    "url": "https://github.com/zmoney20/sample-project"
+        "title": "Node.js Sample Profile Wesbite",
+        "image": "nodejsproject.png",
+        "description": "Sample profile website made with Node.js",
+        "url": "https://github.com/zmoney20/sample-project"
     },
     {
-    "title": "Space Invaders",
-    "image": "space-invaders.png",
-    "description": "Pew pew the alien invaders!",
-    "url": "https://github.com/zmoney20/space-invaders"
+        "title": "Space Invaders",
+        "image": "space-invaders.png",
+        "description": "Pew pew the alien invaders!",
+        "url": "https://github.com/zmoney20/space-invaders"
     },
     {
-    "title": "Fall Game",
-    "image": "fall-game.png",
-    "description": "Play as an 8-bit character sliding and falling into the abyss.",
-    "url": "https://github.com/zmoney20/Fall_game"
+        "title": "Fall Game",
+        "image": "fall-game.png",
+        "description": "Play as an 8-bit character sliding and falling into the abyss.",
+        "url": "https://github.com/zmoney20/Fall_game"
     }
 ]
 
@@ -79,4 +79,4 @@ skills = [
     'jenkins',
     'vim',
     'vault'
-        ]
+]
